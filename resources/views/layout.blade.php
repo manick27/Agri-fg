aaa30b4594d685725a19e4dffc00c9d90158569e
nguewouo mabkop manick <nguewouom@gmail.com>
2022-09-30 17:20:52 +0200

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,19 +16,19 @@
     <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('asset/img/apple-touch-icon-114x114.png') }}">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('//asset/css/bootstrap.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('asset/fonts/font-awesome/css/font-awesome.css') }}">
 
     <!-- Slider
     ================================================== -->
-    <link href="{{ asset('asset/css/owl.carousel.css') }}" rel="stylesheet" media="screen">
-    <link href="{{ asset('asset/css/owl.theme.css') }}" rel="stylesheet" media="screen">
+    <link href="{{ asset('//asset/css/owl.carousel.css') }}" rel="stylesheet" media="screen">
+    <link href="{{ asset('//asset/css/owl.theme.css') }}" rel="stylesheet" media="screen">
 
     <!-- Stylesheet
     ================================================== -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/style.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/nivo-lightbox/nivo-lightbox.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('asset/css/nivo-lightbox/default.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('//asset/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('//asset/css/nivo-lightbox/nivo-lightbox.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('//asset/css/nivo-lightbox/default.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
 
